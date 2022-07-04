42d6d0aa586680af3527b55dbc5cf942f6f363b3
Morgana Weber <50406261+morgsweber@users.noreply.github.com>
2022-07-04 10:56:28 -0300

    Player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,22 +1,38 @@
 extends Area2D
 
-var gravityValue = 1000
-var skip = -800
-var limit = 600
+var gravityValue = 2500
+var skip = -1000
+var limit = 550
 var velocity = Vector2()
+var inFloor = false
+var slow = false
+var fGrav = 3
+var nSkip = 2
+var skipA = 0
 
 func _ready() -> void:
 	pass 
 	
-
 func _physics_process(delta):
-	velocity.y += gravityValue * delta
+	if Input.is_action_just_pressed("skip") and slow:
+		velocity.y += gravityValue * delta
+	else: 
+		velocity.y += gravityValue * delta * fGrav
+		slow = false
 	
-	if Input.is_action_just_pressed("skip"):
-		velocity.y = skip
+	if Input.is_action_just_pressed("skip") and skipA < nSkip:
+		velocity.y = skip	
+		$PlayerSprite.play("jumping")
+		skipA += 1
 		
 	position += velocity * delta
 	
 	if position.y >= limit:
 		position.y = limit
+		slow = true
+		skipA = 0
 		
+		$PlayerSprite.play("running")
+
+func _on_Player_area_entered(area: Area2D) -> void:
+	pass 
